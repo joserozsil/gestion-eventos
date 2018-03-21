14e9b77376dfbbaed015654d3532f05e50f0795c
Jose Rodriguez <jseanvzla@gmail.com>
2018-03-21 19:42:24 -0400

    [Config] Variables de entorno en URL y PUERTO

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-export const SECRET = process.env.PORT || 'SECRET KEY HERE'
+export const SECRET = process.env.SECRET || "SECRET KEY HERE"
 
 export const SETTINGS = {
     "PROD_MODE": false,
-    "APP_PORT": "8080",
-    "DB_URL": "http://localhost:3789/schema"
+    "APP_PORT": process.env.APP_PORT || "8080",
+    "DB_URL": process.env.DB_URL || "http://localhost:3789/schema"
 }
