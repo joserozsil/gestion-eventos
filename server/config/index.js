155c2c577ca7b243a372bdacdce2297a38f4be77
Jose Rodriguez <jseanvzla@gmail.com>
2018-03-22 14:09:09 -0400

    [Config] Modo de mantenimiento

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,8 @@
 export const SECRET = process.env.SECRET || "SECRET KEY HERE"
 
 export const SETTINGS = {
-    "PROD_MODE": false,
+    "PROD_MODE": process.env.PROD_MODE || false,
+    "MANT_MODE": process.env.MANT_MODE || false,
     "APP_PORT": process.env.APP_PORT || "8080",
     "DB_URL": process.env.DB_URL || "http://localhost:3789/schema"
 }
