6395e5b8990b8207b5d5a963a1139a6b77bac312
Jose Rodriguez <joseasve@gmail.com>
2018-06-12 19:52:05 -0400

    Instalacion de nuevo template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
 
