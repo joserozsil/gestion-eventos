d4c16471413b91622ed554752ef1f754b3e8b8dd
Jose Rodriguez <joseasve@gmail.com>
2018-08-08 11:23:04 -0400

    Preparación de Producción

--- server/index.js
+++ server/index.js
@@ -7,13 +7,21 @@ import { settings } from './config'
 
 mongoose.Promise = global.Promise
 
+/*
 mongoose.connect(settings.MONGO_DB || `mongodb://localhost:27017/${settings.DB_NAME}`)
 .then(() => {
     app.listen(settings.APP_PORT, () => {
         console.log(chalk.cyan(`Servidor Node y Express corriendo en el puerto ${ settings.APP_PORT } `))
         console.log(chalk.cyan(`Mongo DB mongodb://localhost:27017/${settings.DB_NAME}`))
     })
+})*/
+
+app.listen(80, () => {
+    console.log(chalk.cyan(`Servidor Node y Express corriendo en el puerto ${ settings.APP_PORT } `))
+    console.log(chalk.cyan(`Mongo DB mongodb://localhost:27017/${settings.DB_NAME}`))
 })
-.catch((err) => console.log(chalk.red(`Error al conectar: ${err}`)))
+
+/*
+.catch((err) => console.log(chalk.red(`Error al conectar: ${err}`)))*/
 
 
