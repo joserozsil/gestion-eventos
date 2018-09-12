2131f3e6c86608be62607a11987e27e2f9083b50
Jose Rodriguez <joseasve@gmail.com>
2018-09-12 16:26:59 -0400

    Permite rendedirzar reporte de retratos

---
+++ report/app/Http/Controllers/ReportController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function portrait(Request $request)
+    {
+
+    	$view = \View::make('portrait')->render();
+
+        $pdf = \App::make('dompdf.wrapper');
+
+        $pdf->loadHTML($view);
+
+        $ROUTE_FILE = 'portraits/' . $this->generateRandomString() . '.pdf';
+
+        $pdf->save($ROUTE_FILE);
+
+        return $ROUTE_FILE;
+
+		//return $pdf->stream();
+    }
+
+    private function generateRandomString($length = 50) { 
+	    return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length); 
+	}
+}
---
+++ report/resources/views/portrait.blade.php
@@ -0,0 +1,249 @@
+
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Retrato Hablado</title>
+    </head>
+    <style>
+        body, table, tr, p {
+            margin: 0;
+        }
+        table {
+            margin: 0;
+        }
+        
+        .title {
+            font-size: 22px;
+        }
+        .uppercase {
+            text-transform: uppercase; 
+        }
+
+        .label {
+        }
+
+        .align-r {
+            text-align: right;
+        }
+    </style>
+    <body>
+        <table style="width:100%">
+          <tr>
+            <td><p class=" uppercase label">Clise: Prueba de expediente</p></td>
+            <td><p class=" uppercase label align-r">Exp: Prueba de expediente</p></td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 5mm 0">
+          <tr>
+            <td>
+                <img style="margin-left: 15mm" src="cicpc.jpg" height="100px">
+            </td>
+            <td width="75%" >
+                <h3 style="margin: 0" class="uppercase title">Delegación Estadal Bolívar</h3>
+                <p style="margin: 0" class="uppercase">Area de análisis y reconstrucción de hechos</p>
+            </td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 5mm 0">
+          <tr>
+            <td width="100%" style="text-align: center;">
+                <img src="retrato.jpg" height="450px">
+            </td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 10mm 0; border: 1px solid gray">
+          <tr>
+            <td>
+                <p class="uppercase"><strong>dependencia:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> Prueba de dependencia y uego otra prueba</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>dependencia:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> Prueba de dependencia y uego otra prueba</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>solicitado por:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> Prueba de dependencia y uego otra prueba</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>agraviado:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">jose rodriguez</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>dependencia:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> Prueba de dependencia y uego otra prueba</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>dirección s.s:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> Prueba de dependencia y uego otra prueba</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>fecha del caso:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>datos por:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 10mm 0;">
+          <tr>
+            <td>
+                <p class="uppercase"><strong>Página: (1/2)</strong></p>
+            </td>
+          </tr>
+        </table>
+    </body>
+    <body>
+        <table style="width:100%">
+          <tr>
+            <td><p class=" uppercase label">Clise: Prueba de expediente</p></td>
+            <td><p class=" uppercase label align-r">Exp: Prueba de expediente</p></td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 10mm 0; border: 1px solid gray">
+          <tr>
+            <td>
+                <p class="uppercase"><strong>Edad:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">18</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>color de piel:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">trigueña</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>boca:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> aaaa</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>labio:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">bbbb</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>estatura:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">ccccc</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>color de cabello:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">ddddd</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>peso:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">adwdw</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>barba-bigote:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>nariz:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">adwdw</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>ojos:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>cara:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">adwdw</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>color de piel:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+                <p class="uppercase"><strong>contextura:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase">adwdw</p>
+            </td>
+            <td>
+                <p class="uppercase"><strong>tipo de cabello:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase"> 16/07/2015</p>
+            </td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 10mm 0; border: 1px solid gray">
+          <tr>
+            <td>
+                <p class="uppercase"><strong>comentario:</strong></p>
+            </td>
+            <td>
+                <p class="uppercase" style="padding: 10px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex porro autem, ratione reprehenderit velit beatae odio laudantium quae, esse quibusdam molestias. Culpa nam eveniet, praesentium numquam est harum eligendi voluptates.</p>
+            </td>
+          </tr>
+        </table>
+        <table style="width:100%; margin: 10mm 0;">
+          <tr>
+            <td>
+                <p class="uppercase"><strong>Página: (2/2)</strong></p>
+            </td>lass="uppercase">(2 de 2)</p>
+            </td>
+          </tr>
+        </table>
+    </body>
+</html>
+
